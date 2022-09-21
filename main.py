d20cdc385cbf312b0c51a045ea9922f996610819
Lasantha <lasantha1130@gmail.com>
2022-09-21 10:12:39 +0530

    finish program

--- main.py
+++ main.py
@@ -8,30 +8,33 @@ def main():
     root.title("Disappearing text app")
     root.geometry("1000x600")
 
-    global text
-    text = Text(root, height=40, width=100, font=25)
-    text.pack(padx=20, pady=20)
+    global text_box
+    text_box = Text(root, height=40, width=100, font=25)
+    text_box.focus()
+    text_box.pack(padx=20, pady=(20, 20))
 
-    root.bind('<KeyPress>', on_press)
+    # When that pressed key is released
     root.bind('<KeyRelease>', on_release)
 
     root.mainloop()
 
 
-def on_press(key):
-    try:
-        root.after_cancel(delete_id)
-    except NameError:
-        pass
-
-
+# When a key is released delete function is going to get call after 5 seconds
 def on_release(key):
-    global delete_id
-    delete_id = root.after(5000, delete_text)
+    text_before = text_box.get("1.0", END)
+    root.after(5000, delete_text, text_before)
 
 
-def delete_text():
-    text.delete("1.0", END)
+def delete_text(text):
+    text_before = text
+    text_now = text_box.get("1.0", END)
+
+    """
+    When after calls the delete text function.
+    If before letter count == now letter count text every letter in the textbox is going to get cleared.
+    """
+    if len(text_before) == len(text_now):
+        text_box.delete("1.0", END)
 
 
 main()
